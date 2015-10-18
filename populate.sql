d3227774323cb1e1516ce0a5c7bfe93110285af8
lindaZ1 <dzhang4@ualberta.ca>
2015-10-18 13:54:45 -0600

    populate.sql does not contain sensor type data

---
+++ populate.sql
@@ -0,0 +1,27 @@
+INSERT INTO persons 
+VALUES ('111', 'Abby','Apple','123 Apple St','aa@gmail.com','1234567'),
+       ('222', 'Brady', 'Bunch', '123 Happy Ave', 'bb@gmail.com','1234568'),
+       ('333', 'Charlie','Charcoal','107 Tin Rd', 'cc@gmail.com','1234569'),
+       ('444', 'Dan','Danger','1388 Soma Dr','dd@gmail.com','1234566')
+
+INSERT INTO users
+VALUES ('user1','pass1','a','111','2002-02-02'),
+       ('user12','pass12','d','111','2002-02-02'),
+       ('user2','pass2','s','222','2003-07-07'),
+       ('user3','pass3','d','333','2005-3-13'),
+       ('user4','pass4','s','333','2004-09-09')
+
+INSERT INTO sensors
+VALUES ('1111','ocean','a','oceanssss'),
+       ('2222','forest','i','forestssss'),
+       ('3333','house','t','housessss'),
+       ('4444','fort','o','fortssssss')
+
+INSERT INTO subscriptions
+VALUES ('1111','222'),
+       ('2222','222'),
+       ('2222','333'),
+       ('3333','222'),
+       ('4444','333') 
+
+
